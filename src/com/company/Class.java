10beaab5ffad23216f5ead5d3fc59bac39e358d4
Jamie Hewitt <jamie.hewitt15@gmail.com>
2020-01-04 10:36:54 +0000

    Class selection bug fix

--- src/com/company/Class.java
+++ src/com/company/Class.java
@@ -3,22 +3,17 @@ package com.company;
 public class Class {
     private String className;
     ///private Rating rating;
-    private int classCapacity = 20;
     private String classTime = "Unspecified";
     private int classPrice = 20;
-    private boolean[] classWeekend = {false, false, false, false};
-    private boolean[] classDay = {false, false};
-    private boolean daySaturday = false;
-    private boolean daySunday = false;
+    private boolean[] classDay = {false, false, false, false, false, false, false, false};
+    private int[] classAttendance = {0, 0, 0, 0, 0, 0, 0, 0};
 
-    public Class(String name, String time, int price,  boolean[] weekend, boolean[] day, boolean saturday, boolean sunday) {
+
+    public Class(String name, String time, int price,  boolean[] day) {
         this.setClassName(name);
         this.setClassTime(time);
         this.setClassPrice(price);
-        this.setWeekend(weekend);
         this.setDay(day);
-        this.setSaturday(saturday);
-        this.setSunday(sunday);
         ///rating=new Rating();
     }
 
@@ -39,58 +34,22 @@ public class Class {
     {
         this.classPrice = price;
     }
-    public void setWeekend(boolean[] weekend)
-    {
-        this.classWeekend = weekend;
-    }
     public void setDay(boolean[] day) { this.classDay = day; }
-    public void setSaturday(boolean saturday)
-    {
-        this.daySaturday = saturday;
-    }
-    public void setSunday(boolean sunday)
-    {
-        this.daySunday = sunday;
-    }
     ///public void setRating(Rating rate)
     {
        // this.rating = rate;
     }
 
-    public boolean getClassDay(int i) {
-        return classDay[i];
-    }
-
-    public boolean getClassWeekend(int i) {
-        return classWeekend[i];
-    }
-
-    public boolean getSaturday() {
-        return this.daySaturday;
-    }
-
-    public boolean getSunday() {
-        return this.daySunday;
-    }
-
+    public boolean getClassDay(int i) { return this.classDay[i]; }
+    public int getClassAttendance(int i) { return this.classAttendance[i]; }
     public String getClassName() {
         return className;
     }
-
     public int getClassPrice() {
         return this.classPrice;
     }
-
-    public int getClassCapacity() {
-        return this.classCapacity;
-    }
-
     public String getClassTime() {
         return this.classTime;
-    }
-
-    public boolean getClassDay(){
-        return this.daySaturday;
     }
 
     //public Rating getRating()
--- src/com/company/Schedule.java
+++ src/com/company/Schedule.java
@@ -5,37 +5,45 @@ import java.util.Scanner;
 
 
 public class Schedule {
-    private final int MaxPerDay = 4; // Maximum number of classes per day
-    private Class[] schedule = new Class[6];
-//    private Class[] sundayClasses = new Class[MaxPerDay];
+    private final int MaxClassesPerDay = 4; // Maximum number of classes per day
+    private final int classCapacity = 20; // Maximum number of people in a class
+    String[] openDays = {   "Saturday, 1st February", "Sunday, 2nd February",
+                            "Saturday, 8th February",  "Sunday, 9th February",
+                            "Saturday, 15th February", "Sunday, 16th February",
+                            "Saturday, 22nd February","Sunday, 23nd February"
+                        };
 
-//    String[][] className = {
-//            {"English", "Saturday", "9:00", "£20", },
-//            {"Math", "Saturday", "15:00", "£10"},
-//            {"Verbal Reasoning", "Saturday", "19:00", "£12"},
-//            {"Non-verbal Reasoning", "Saturday", "20:00", "£20"},
-//
-//            {"Science", "Sunday", "10:00", "£15"},
-//            {"Math", "Sunday", "15:00", "£10"},
-//            {"Verbal Reasoning", "Sunday", "19:00", "£12"},
-//            {"Programming", "Sunday", "20:00", "£25"},
-//    };
+
+    private Class[] schedule = new Class[12];
+
 
     public Schedule() {
 
-        Class english = new Class("English", "9:00", 20, new boolean[]{true, true, true, true}, new boolean[]{true, false}, true, false);
-        Class math = new Class("Maths", "15:00", 10, new boolean[]{true, true, true, true}, new boolean[]{true, true}, true, true);
-        Class verbalReasoning = new Class("Verbal Reasoning", "19:00", 12, new boolean[]{true, true, true, true}, new boolean[]{true, true}, true, true);
-        Class nonVerbalReasoning = new Class("Non-verbal Reasoning", "20:00", 16, new boolean[]{true, true, true, true}, new boolean[]{true, false}, true, false);
-        Class science = new Class("Science", "10:00", 15, new boolean[]{true, true, true, true}, new boolean[]{false, true}, false, true);
-        Class programming = new Class("Programming", "20:00", 25, new boolean[]{true, true, true, true}, new boolean[]{false, true}, false, true);
+        Class english = new Class("English", "9:00", 20, new boolean[]{true, false, true, false, false, false, false, false});
+        Class maths = new Class("Maths", "15:00", 10, new boolean[]{true, true, true, true, false, false, false, false});
+        Class verbalReasoning = new Class("Verbal Reasoning", "19:00", 12, new boolean[]{true, true, true, true, false, false, false, false});
+        Class nonVerbalReasoning = new Class("Non-verbal Reasoning", "20:00", 16, new boolean[]{true, true, true, true, false, false, false, false});
+        Class biology = new Class("Biology", "10:00", 15, new boolean[]{false, true, false, true, false, false, false, false });
+        Class programming = new Class("Programming", "20:00", 25, new boolean[]{false, true, false, true, false, false, false, false});
+        Class french = new Class("French", "9:00", 20, new boolean[]{ false, false, false, false, true, false, true, false});
+        Class german = new Class("German", "15:00", 10, new boolean[]{false, false, false, false, true, true, true, true});
+        Class history = new Class("History", "19:00", 12, new boolean[]{false, false, false, false, true, true, true, true});
+        Class economics = new Class("Economics", "20:00", 16, new boolean[]{false, false, false, false, true, false, true, false});
+        Class geography = new Class("Geography", "10:00", 15, new boolean[]{false, false, false, false, false, true, false, true});
+        Class physics = new Class("Physics", "20:00", 25, new boolean[]{false, false, false, false, false, true, false, true});
 
         schedule[0] = english;
-        schedule[1] = math;
+        schedule[1] = maths;
         schedule[2] = verbalReasoning;
         schedule[3] = nonVerbalReasoning;
-        schedule[4] = science;
+        schedule[4] = biology;
         schedule[5] = programming;
+        schedule[6] = french;
+        schedule[7] = german;
+        schedule[8] = history;
+        schedule[9] = economics;
+        schedule[10] = geography;
+        schedule[11] = physics;
     }
 
 
@@ -43,55 +51,41 @@ public class Schedule {
     static void book() {
 
         Schedule menuClasses = new Schedule();
-        Scanner classWeekend = new Scanner(System.in);  // Create a Scanner object
-        System.out.println("\nWhich weekend would you like to book your class on?" +
-                "\n1 : 1st & 2nd February\n2 : 8th & 9th February\n3 : 15th & 16th February\n4 : 22nd & 23rd February");
-        int weekendChoice = (classWeekend.nextInt() - 1);
-
-        System.out.println("\nWhat day would you like to book your class on?\n1 : Saturday\n2 : Sunday");
         Scanner classDay = new Scanner(System.in);  // Create a Scanner object
-        int classDayChoice = (classDay.nextInt() - 1);  // Read user input
+        System.out.println("\nWhat day would you like to book your class on?\n");
+        int dayCount = 1;
+        for (String i : menuClasses.openDays) {
+            System.out.println(dayCount +": "+i); // Show options for days when classes can be booked.
+            dayCount++;
+        }
+        int dayChoice = (classDay.nextInt() - 1);
+//
+//        System.out.println("\nWhat day would you like to book your class on?\n1 : Saturday\n2 : Sunday");
+//        Scanner classDay = new Scanner(System.in);  // Create a Scanner object
+//        int classDayChoice = (classDay.nextInt() - 1);  // Read user input
+//
         int count = 1;
+
 
         System.out.println("\nSchedule:"); // code block
         for (int i = 0; i < menuClasses.schedule.length; i++) {
-            if (menuClasses.schedule[i].getClassDay(classDayChoice) && menuClasses.schedule[i].getClassWeekend(weekendChoice)) {
-                System.out.println("\n"+ count + " : " + menuClasses.schedule[i].getClassName() + " at " + menuClasses.schedule[i].getClassTime() + " | Cost: £" + menuClasses.schedule[i].getClassPrice());
-                count++;
-            }
+            if ((menuClasses.schedule[i].getClassDay(dayChoice) == true) && (count <= menuClasses.MaxClassesPerDay)) {
+                  System.out.println("\n"+ i + " : " + menuClasses.schedule[i].getClassName() + " at " + menuClasses.schedule[i].getClassTime() + " | Cost: £" + menuClasses.schedule[i].getClassPrice());
+                  count++;
+                }
+
         }
 
-
-//        switch(classDayChoice) {
-//
-//            case 1:
-//                System.out.println("Sunday schedule:"); // code block
-//                for (int i = 0; i < menuClasses.schedule.length; i++) {
-//                    if (menuClasses.schedule[i].getSaturday() && menuClasses.schedule[i].getClassWeekend(weekendChoice)) {
-//                    System.out.println("\n"+ count + " : " + menuClasses.schedule[i].getClassName() + " at " + menuClasses.schedule[i].getClassTime() + " | Cost: £" + menuClasses.schedule[i].getClassPrice());
-//                    count++;
-//                    }
-//                }
-//                break;
-//            case 2:
-//                System.out.println("Saturday schedule:"); // code block
-//                for (int i = 0; i < menuClasses.schedule.length; i++) {
-//                    if (menuClasses.schedule[i].getSunday()&& menuClasses.schedule[i].getClassWeekend(weekendChoice)) {
-//                        System.out.println("\n"+ count + " : " + menuClasses.schedule[i].getClassName() + " at " + menuClasses.schedule[i].getClassTime() + " | Cost: £" + menuClasses.schedule[i].getClassPrice());
-//                        count++;
-//                    }
-//                }
-//                break;
-//        }
 
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
         System.out.println("\nEnter the number to book the class:");
 
-        int classChoice = myObj.nextInt() -1;  // Read user input
+        int classChoice = myObj.nextInt();  // Read user input
 
         try {
             System.out.println("\n-------------------------------------------------" +
                     "\nYou have booked class: " + menuClasses.schedule[classChoice].getClassName() + " at " + menuClasses.schedule[classChoice].getClassTime()
+                    + "\nDate: " + menuClasses.openDays[dayChoice]
                     +  "\nThe price will be: £" + menuClasses.schedule[classChoice].getClassPrice()
                     + "\n-------------------------------------------------");  // Output user input
         } catch (Exception e){
